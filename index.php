34134cda1681c708a0b91e515095d2f1737ff7cf
Tony <tony.g.thompson@gmail.com>
2018-01-12 16:27:54 -0800

    Added error reporting

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
+// Turn on error reporting
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
 
 require_once ('vendor/autoload.php');
 
 $f3 = Base::instance();
+
+//set debug level
+$f3->set('DEBUG', 3);
 
 $f3 -> route('GET /', function() {
     echo "Pet Home!";
